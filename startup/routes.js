9b92f98be6d17c7945a5376b5e1b6769ef3d5eee
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-11-11 21:36:15 +0200

    Add cors middleware

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
+var cors = require("cors");
 const categories = require("../routes/categories");
 
 module.exports = app => {
   app.use(express.json());
+  app.use(cors());
   app.use("/api/categories", categories);
 };
