22831194d38110c829a2b53421b8e05849620c4d
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-11-26 02:13:27 +0200

    Add get image route

--- models/static.js
+++ models/static.js
@@ -1,18 +1,27 @@
+const fs = require("fs");
+const path = require("path");
 const config = require("config");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, `./static/${req.body.path || ""}`),
+  destination: (req, file, cb) => cb(null, "./static/"),
   filename: (req, file, cb) => cb(null, `${Date.now()}.jpg`)
 });
 
 const upload = multer({ storage }).single("image");
 
-const createImage = ({ body, file }) => {
+const createImage = ({ file }) => {
   if (!file) return { status: 400, error: "No image uploaded." };
 
   return { data: { imageUrl: `${config.get("apiHost")}/api/static/${file.filename}` } };
 };
 
+const getImage = ({ params }) => {
+  const imagePath = path.join(__dirname, "../static/", params.name);
+  if (!fs.existsSync(imagePath)) return { status: 404, error: "No such file." };
+  return { data: { imagePath } };
+};
+
 exports.upload = upload;
 exports.createImage = createImage;
+exports.getImage = getImage;
--- routes/static.js
+++ routes/static.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const { upload, createImage } = require("../models/static");
+const { upload, createImage, getImage } = require("../models/static");
 const router = express.Router();
 
 router.post("/", upload, (req, res) => {
   const { data, status, error } = createImage(req);
   if (error) return res.status(status).send(error);
   res.send(data);
+});
+
+router.get("/:name", (req, res) => {
+  const { data, status, error } = getImage(req);
+  if (error) return res.status(status).send(error);
+  res.sendFile(data.imagePath);
 });
 
 module.exports = router;
